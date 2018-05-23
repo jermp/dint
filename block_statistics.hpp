58d00ffc05f41a2f4fd9f25e9fb1cf4ca9c10f4a
Matthias Petri <Matthias.Petri@gmail.com>
2018-05-23 12:02:34 +1000

    reworking some stuff for merge

---
+++ block_statistics.hpp
@@ -0,0 +1,153 @@
+#pragma once
+
+#include "binary_collection.hpp"
+#include "hash_utils.hpp"
+
+#include <boost/progress.hpp>
+
+#include <unordered_map>
+
+const size_t MIN_SIZE_THRES = 256;
+
+namespace ds2i {
+    namespace util {
+        bool is_power_of_two(uint64_t x) {
+            return (x & (x - 1)) == 0;
+        }
+    }
+
+    #pragma pack(push, 1)
+    template<uint32_t width>
+    struct block_info {
+        uint64_t hash;
+        uint64_t freq;
+        uint8_t entry_len;
+        uint32_t entry[width];
+    };
+    #pragma pack(pop)
+
+    template<class bm_type>
+    void update_entry(const uint32_t* entry,size_t n,bm_type& block_map)
+    {
+    	using b_type = typename bm_type::value_type;
+    	auto hash = hash_u32(b,size_u32);
+		auto itr = block_map.find(hash);
+		if(itr != block_map.end())
+			itr->freq++;
+		else {
+			b_type new_block;
+			new_block.hash = hash;
+			new_block.freq = 1;
+			new_block.entry_len = size_u32;
+			std::copy(b,b+size_u32,new_block.entry);
+			block_map[hash] = new_block;
+		}
+    }
+
+    struct stats_geometric {
+    	static std::string type() {
+    		return "geometric";
+    	}
+    	
+    	template<class bm_type>
+    	static size_t update_stats(const std::vector<uint32_t>& buf,bm_type& block_map) {
+    		auto b = buf.data();
+    		for(size_t size_u32=1;size_u32<=buf.size();size_u32*=2) {
+    			update_entry(b,size_u32,block_map);
+    		}
+    	}
+    };
+
+    struct stats_adjusted {
+    	static std::string type() {
+    		return "adjusted";
+    	}
+
+    	template<class bm_type>
+    	static void update_stats(std::vector<uint32_t>& buf,bm_type& block_map) {
+    		auto b = buf.data();
+    		for(size_t size_u32=1;size_u32<=buf.size();size_u32*=2) {
+    			for(size_t pos=0;pos<buf.size();pos+=size_u32) {
+    				update_entry(b+pos,size_u32,block_map);
+    			}
+    		}
+    	}
+    };
+
+    template<uint32_t max_entry_width,class stats_type>
+    struct block_statistics {
+    	static_assert(util::is_power_of_two(max_entry_width));
+    	using block_type = block_info<max_entry_width>;
+    	using bm_type = std::unordered_map<uint64_t,block_type>;
+
+        static std::string type() {
+            return "block_stats-L" + std::to_string(max_entry_width) + "-" + stats_type::type();
+        }
+
+    	block_statistics(binary_collection& input,bool compute_gaps) {
+            logger() << "creating block stats" << std::endl;
+            bm_type block_map;
+            block_map.max_load_factor(0.01);
+            boost::progress_display progress(input.data_size());
+            for (auto const& list: input) {
+                size_t n = list.size();
+                progress += n+1;
+                if (n < MIN_SIZE_THRES) continue;
+                process_list(block_map,list,compute_gaps);
+            }
+
+            logger() << "serializing stats" << std::endl;
+            blocks.resize(block_map.size());
+            auto block_selector = [](auto& pair){return pair.second;};
+            std::transform(block_map.begin(), block_map.end(),blocks.begin(), block_selector);
+            logger() << "sort by freq" << std::endl;
+            auto freq_cmp = [](const auto& a,const auto& b) {return a.freq > b.freq;};
+            std::sort(blocks.begin(),blocks.end(),freq_cmp);
+            logger() << "done creating stats" << std::endl;
+    	}
+
+        block_statistics(std::string file_name) {
+            std::ifstream in(file_name.c_str());
+            uint64_t num_blocks;
+            in.read(reinterpret_cast<char*>(&num_blocks), sizeof(uint64_t));
+            logger() << "reading block stats (num_blocks = " << num_blocks << ")" << std::endl;
+            blocks.resize(num_blocks);
+            auto block_data = reinterpret_cast<char*>(blocks.data());
+            in.read(block_data, num_blocks * sizeof(block_type));
+        }
+
+    	void process_list(bm_type& block_map,t_list& list,bool compute_gaps) {
+    		thread_local std::vector<uint32_t> buf(max_entry_width);
+            size_t n = list.size();
+            size_t full_strides = n / max_entry_width;
+            auto lst_itr = list.begin();
+            uint32_t prev = 0;
+            for(size_t i=0;i<full_strides;i++) {
+                // (1) fill buf
+                for(size_t j=0;j<max_entry_width;j++) {
+                    buf[j] = *lst_itr - prev;
+                    if(compute_gaps == true) prev = *lst_itr;
+                    ++lst_itr;
+                }
+                // (2) process buf and update freq stats
+                stats_type::update_stats(buf,block_map);
+            }
+    	}
+
+        void try_to_store(std::string file_name) {
+            std::ofstream out(file_name.c_str());
+            if(out) {
+            	logger() << "writing block stats" << std::endl;
+                uint64_t num_blocks = blocks.size();
+                out.write(reinterpret_cast<char const*>(&num_blocks), sizeof(uint64_t));
+                out.write(reinterpret_cast<char const*>(blocks.data()), num_blocks* sizeof(block_type));
+            } else {
+            	logger() << "cannot write block stats. collection directory not writeable" << std::endl;
+            }
+        }
+
+    	std::vector<block_type> blocks;
+    };
+
+}
+
--- create_dint_dict.cpp
+++ create_dint_dict.cpp
@@ -9,7 +9,7 @@
 #include <boost/progress.hpp>
 
 #include "dictionary_builders.hpp"
-#include "block_stats.hpp"
+#include "block_statistics.hpp"
 #include "block_codecs.hpp"
 
 using namespace ds2i;
@@ -42,217 +42,6 @@ ds2i::dictionary::builder build_dict(std::ostream& os,block_stat_type& block_sta
     return dict_builder;
 }
 
-struct block_enc_stats {
-    std::vector<int64_t> dict_usage_lens;
-    std::vector<int64_t> dict_usage;
-    std::vector<int64_t> dict_entry_lens;
-    std::vector<int64_t> dict_entry_freqs;
-    std::vector<uint32_t> codes_per_block;
-    std::vector<uint32_t> exceptions_per_block;
-    std::vector<int32_t> block_size;
-    std::vector<std::string> dict_entries;
-    size_t num_blocks = 0;
-    size_t total_codes = 0;
-    size_t total_exception_codes_u16 = 0;
-    size_t total_exception_codes_u32 = 0;
-    size_t postings_encoded = 0;
-
-    void init(ds2i::dictionary::builder& dict) {
-        size_t capacity = dict.capacity();
-        for(size_t i=0;i<capacity;i++) {
-            dict_entry_freqs.push_back(dict.freq(i));
-        }
-        dict_usage.resize(capacity);
-        dict_entry_lens.resize(capacity);
-        dict_usage_lens.resize(256+1);
-        dict_entry_lens[0] = 0;
-        dict_entries.push_back("[exception code 16bit]");
-        dict_entry_lens[1] = 0;
-        dict_entries.push_back("[exception code 32bit]");
-        dict_entry_lens[2] = 256;
-        dict_entries.push_back("[1]*256");
-        dict_entry_lens[3] = 128;
-        dict_entries.push_back("[1]*128");
-        dict_entry_lens[4] = 64;
-        dict_entries.push_back("[1]*64");
-        dict_entry_lens[5] = 32;
-        dict_entries.push_back("[1]*32");
-        for(size_t i=6;i<capacity;i++) {
-            dict_entry_lens[i] = dict.size(i);
-            dict_entries.push_back(dict.entry_string(i));
-        }
-
-    }
-
-    template<class t_dict>
-    void update(t_dict& dict,size_t input_size,std::vector<uint32_t>& codes,size_t num_codes,size_t num_exceptions)
-    {
-        num_blocks++;
-        postings_encoded += input_size;
-        total_codes += num_codes;
-        block_size.push_back(input_size);
-        codes_per_block.push_back(num_codes+num_exceptions);
-        exceptions_per_block.push_back(num_exceptions);
-        for(size_t i=0;i<num_codes;i++) {
-            dict_usage[codes[i]]++;
-            if(codes[i] < dict.special_cases()) {
-                switch(codes[i]) {
-                    case 0:
-                        dict_usage_lens[0]++;
-                        total_exception_codes_u16++;
-                        break;
-                    case 1:
-                        dict_usage_lens[1]++;
-                        total_exception_codes_u32++;
-                        break;
-                    case 2:
-                        dict_usage_lens[256]++;
-                        break;
-                    case 3:
-                        dict_usage_lens[128]++;
-                        break;
-                    case 4:
-                        dict_usage_lens[64]++;
-                        break;
-                    case 5:
-                        dict_usage_lens[32]++;
-                        break;
-                }
-            } else {
-                dict_usage_lens[dict.size(codes[i])]++;
-            }
-
-        }
-    }
-
-    void print(std::ostream &os,ds2i::dictionary::builder& dict) {
-        os << "DICT LEN USAGE:\n";
-        for(size_t i=0;i<dict_usage_lens.size();i++) {
-            if(dict_usage_lens[i] != 0) {
-                size_t encoded_nums = i;
-                if(i == 0) encoded_nums = 1;
-                os  << "\tlen = " << std::setw(3) << i
-                    << "\tnum_codes = "  << std::setw(10) << dict_usage_lens[i]
-                    << "\tnum_postings = " << std::setw(10) << dict_usage_lens[i] * encoded_nums
-                    << "\tpercent of codes = " << std::setw(6) << std::fixed <<  std::setprecision(2) << double(dict_usage_lens[i]) / double(total_codes) * 100
-                    << "\tpercent of postings = " << std::setw(6) << std::fixed <<  std::setprecision(2) << double(dict_usage_lens[i]*encoded_nums) / double(postings_encoded) * 100 << "\n";
-            }
-        }
-        os << "CODE WORD USAGE:\n";
-        size_t total_bits = 0;
-        for(size_t i=0;i<dict_usage.size();i++) {
-            int64_t encoded_nums = dict.size(i);
-            int64_t shorts_used = 1;
-            if(i == 0 || i == 1) {
-                encoded_nums = 1;
-                shorts_used = 2;
-                if(i == 1) shorts_used = 3;
-            }
-            size_t cur_bits = dict_usage[i] * shorts_used * 16;
-            total_bits += cur_bits;
-            os  << "\tcode = " << std::setw(5) << i
-                << "\tcode_u16 = " << std::setw(1) << shorts_used
-                << "\tentry_len = " << std::setw(3) << dict.size(i)
-                << "\tfreq = " << std::setw(12) << dict_usage[i]
-                << "\tpredicted_freq = " << std::setw(12) << dict_entry_freqs[i]
-                << "\tpercent of codes = " << std::setw(6) << std::fixed << std::setprecision(2) << double(dict_usage[i]) / double(total_codes) * 100
-                << "\tpercent of postings = " << std::setw(6) << std::fixed <<  std::setprecision(2) << double(dict_usage[i]*encoded_nums) / double(postings_encoded) * 100
-                << "\t" << dict.entry_string(i) << "\n";
-        }
-
-        std::sort(codes_per_block.begin(),codes_per_block.end());
-
-        os << "CODES PER BLOCK:\n";
-        size_t cur = codes_per_block[0];
-        size_t freq = 1;
-        for(size_t i=1;i<codes_per_block.size();i++) {
-            if(codes_per_block[i] != cur) {
-                os  << "\t\tnum_codes_in_block = " << std::setw(5) << cur
-                    << "\tbpi = " << std::setw(6) << std::fixed <<  std::setprecision(3) << double(cur*16) / double(block_size[0])
-                    << "\tfreq = " << std::setw(10) << freq
-                    << "\tpercent of blocks = " << std::setw(6) << std::fixed <<  std::setprecision(2) << double(freq) / double(num_blocks) * 100 << "\n";
-                freq = 1;
-            } else {
-                freq++;
-            }
-            cur = codes_per_block[i];
-        }
-
-        std::sort(exceptions_per_block.begin(),exceptions_per_block.end());
-
-        os << "EXCEPTIONS PER BLOCK:\n";
-        cur = exceptions_per_block[0];
-        freq = 1;
-        for(size_t i=1;i<exceptions_per_block.size();i++) {
-            if(exceptions_per_block[i] != cur) {
-                os  << "\t\texceptions_per_block = " << std::setw(5) << cur
-                    << "\tfreq = " << std::setw(10) << freq
-                    << "\tpercent of blocks = " << std::setw(6) << std::fixed <<  std::setprecision(2) << double(freq) / double(num_blocks) * 100 << "\n";
-                freq = 1;
-            } else {
-                freq++;
-            }
-            cur = exceptions_per_block[i];
-        }
-
-        os << "RBO = \n";
-
-        struct rbo_entry {
-            size_t id;
-            size_t freq;
-            size_t predicted_freq;
-            bool operator<(const rbo_entry& other) {
-                if(freq == other.freq) return id < other.id;
-                return freq > other.freq;
-            }
-        };
-
-        std::vector<rbo_entry> rbo_list;
-        for(size_t i=dict.special_cases();i<dict_usage.size();i++) {
-            rbo_entry r;
-            r.id = i;
-            r.freq = dict_usage[i];
-            r.predicted_freq = dict_entry_freqs[i];
-            rbo_list.push_back(r);
-        }
-        std::sort(rbo_list.begin(),rbo_list.end());
-        for(size_t i=0;i<rbo_list.size();i++) {
-            os << "rbo," << i << "," << r.id << std::endl;
-        }
-
-        os << "\n\nPOSTINGS = " << postings_encoded  << std::endl;
-        os << "BPI = " <<  std::setprecision(5) << double(total_bits) / double(postings_encoded) << std::endl;
-        os << "TOTAL_NON_EXCEPTION_U16 = " << total_codes  << std::endl;
-        os << "TOTAL_EXCEPTION_U16 = " << total_exception_codes_u16  << std::endl;
-        os << "TOTAL_EXCEPTION_U32 = " << total_exception_codes_u32  << std::endl;
-        os << "TOTAL_U16 = " << total_codes + total_exception_codes_u16 + (2 * total_exception_codes_u32)  << std::endl;
-        os << "EXCEPTIONS BPI = " << double((total_exception_codes_u16 + (2 * total_exception_codes_u32)) * 16) / double(postings_encoded) << std::endl;
-
-    }
-};
-
-
-
-struct encoding_stats {
-    encoding_stats(ds2i::dictionary::builder& dict) {
-        small_lists.init(dict);
-        small_lists.init(dict);
-        full_blocks.init(dict);
-        nonfull_blocks.init(dict);
-        all_blocks.init(dict);
-    }
-
-    block_enc_stats small_lists;
-    block_enc_stats full_blocks;
-    block_enc_stats nonfull_blocks;
-    block_enc_stats all_blocks;
-
-    void print(std::ostream& os,ds2i::dictionary::builder& dict) {
-        all_blocks.print(os,dict);
-    }
-};
-
-
 encoding_stats encode_lists(ds2i::dictionary::builder& dict,std::string input_basename,dict_type type,size_t block_size)
 {
     logger() << "encoding lists" << std::endl;
@@ -275,40 +64,23 @@ encoding_stats encode_lists(ds2i::dictionary::builder& dict,std::string input_ba
         size_t n = list.size();
         auto itr = list.begin();
         uint32_t prev = 0;
-        if(n < block_size) { // small list
-            for(size_t j=0;j<n;j++) {
-                    buf[j] = *itr - prev;
-                    if(type == dict_type::docs) prev = *itr;
-                    ++itr;
+        size_t full_blocks = n / block_size;
+        size_t left = n % block_size;
+        for(size_t i=0;i<full_blocks;i++) {
+            for(size_t j=0;j<block_size;j++) {
+                buf[j] = *itr - prev;
+                if(type == dict_type::docs) prev = *itr;
+                ++itr;
             }
-            auto bs = dint_block::encode(dict,buf.data(),n,output.data());
-            stats.small_lists.update(dict,n,output,bs.written_codes,bs.written_exceptions);
-            stats.all_blocks.update(dict,n,output,bs.written_codes,bs.written_exceptions);
-        } else {
-            size_t full_blocks = n / block_size;
-            size_t left = n % block_size;
-            for(size_t i=0;i<full_blocks;i++) {
-                for(size_t j=0;j<block_size;j++) {
-                    buf[j] = *itr - prev;
-                    if(type == dict_type::docs) prev = *itr;
-                    ++itr;
-                }
-                auto bs = dint_block::encode(dict,buf.data(),block_size,output.data());
-                stats.full_blocks.update(dict,block_size,output,bs.written_codes,bs.written_exceptions);
-                stats.all_blocks.update(dict,block_size,output,bs.written_codes,bs.written_exceptions);
+            size_t written_codes = dint_block::encode(dict,buf.data(),block_size,output.data());
+        }
+        if(left) {
+            for(size_t j=0;j<left;j++) {
+                buf[j] = *itr - prev;
+                if(type == dict_type::docs) prev = *itr;
+                ++itr;
             }
-
-
-            if(left) {
-                for(size_t j=0;j<left;j++) {
-                    buf[j] = *itr - prev;
-                    if(type == dict_type::docs) prev = *itr;
-                    ++itr;
-                }
-                auto bs = dint_block::encode(dict,buf.data(),left,output.data());
-                stats.nonfull_blocks.update(dict,left,output,bs.written_codes,bs.written_exceptions);
-                stats.all_blocks.update(dict,left,output,bs.written_codes,bs.written_exceptions);
-            }
+            size_t written_codes = dint_block::encode(dict,buf.data(),left,output.data());
         }
         progress += n+1;
     }
@@ -321,27 +93,15 @@ void eval_dict(std::string input_basename,std::string log_prefix)
 {
     {
         std::ofstream log_file(log_prefix + "-docs-" + dict_constructor_type::type());
-
         auto block_stats = create_block_stats<block_stat_type>(input_basename,dict_type::docs);
         auto dict = build_dict<block_stat_type,dict_constructor_type>(log_file,block_stats);
-
-        dict.print_stats(log_file);
-
-        auto enc_stats = encode_lists(dict,input_basename,dict_type::docs,encoding_block_size);
-
-        enc_stats.print(log_file,dict);
+        encode_lists(dict,input_basename,dict_type::docs,encoding_block_size);
     }
     {
         std::ofstream log_file(log_prefix + "-freqs-" + dict_constructor_type::type());
-
         auto block_stats = create_block_stats<block_stat_type>(input_basename,dict_type::freqs);
         auto dict = build_dict<block_stat_type,dict_constructor_type>(log_file,block_stats);
-
-        dict.print_stats(log_file);
-
-        auto enc_stats = encode_lists(dict,input_basename,dict_type::freqs,encoding_block_size);
-
-        enc_stats.print(log_file,dict);
+        encode_lists(dict,input_basename,dict_type::freqs,encoding_block_size);
     }
 }
 
@@ -362,19 +122,20 @@ int main(int argc, const char** argv) {
     const uint32_t max_entry_width = 16;
     const uint32_t dict_entries = 65536;
     
-    // // PDF
-    {
-        using block_stat_type = ds2i::block_stats_full_stride_geom<max_entry_width>;
-        using dict_type = ds2i::dint_dict_builder_PDF<block_stat_type,dict_entries, max_entry_width>;
-        eval_dict<dict_type,block_stat_type,encoding_block_size>(input_basename,log_prefix);
-    }
     // DSF
     {
         using block_stat_type = ds2i::block_stats_full_stride_geom<max_entry_width>;
         using dict_type = ds2i::dint_dict_builder_DSF<block_stat_type,dict_entries, max_entry_width>;
         eval_dict<dict_type,block_stat_type,encoding_block_size>(input_basename,log_prefix);
     }
-    // SDF
+    // // // PDF
+    // {
+    //     using block_stat_type = ds2i::block_stats_full_stride_geom<max_entry_width>;
+    //     using dict_type = ds2i::dint_dict_builder_PDF<block_stat_type,dict_entries, max_entry_width>;
+    //     eval_dict<dict_type,block_stat_type,encoding_block_size>(input_basename,log_prefix);
+    // }
+
+    // // SDF
     // {
     //     using block_stat_type = ds2i::block_stats_full_stride_linear<max_entry_width>;
     //     using dict_type = ds2i::dint_dict_builder_SDF<block_stat_type,dict_entries, max_entry_width>;
