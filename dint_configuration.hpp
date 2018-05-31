56252d3fcfcc0e16e8b9d3e1ea9d3622b6481e32
Giulio Ermanno Pibiri <jeis90@gmail.com>
2018-05-31 12:55:38 +1000

    more refactoring

---
+++ dint_configuration.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace ds2i {
+    namespace constants {
+        static const uint64_t min_size = 4096;
+        static const uint32_t max_block_length = 16;
+        static const uint32_t max_fractal_step = 5;
+        static const uint32_t max_entry_width = 16;
+        static const uint32_t num_entries = 65536;
+        static const uint32_t codeword_bits = 16;
+        static const double initial_bpi = 48.0;
+        static const double eps = 0.0001;
+    }
+}
