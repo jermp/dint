6395a69e06fc9b5ef907fc88896d6134223f110c
Giulio Ermanno Pibiri <jeis90@gmail.com>
2018-06-02 13:01:58 +1000

    scripts/build_dint_indexes.py

--- scripts/build_dint_indexes.py
+++ scripts/build_dint_indexes.py
@@ -6,5 +6,7 @@ ouput_prefix_name = sys.argv[2]
 strategies = ["DSF", "PDF", "LSS"]
 
 for strategy in strategies:
-    cmd = "./create_freq_index " + strategy + "_block_dint " + collection_basename + " " + ouput_prefix_name + "." + strategy + ".out"
+    output = ouput_prefix_name + "." + strategy
+    cmd = "./create_freq_index " + strategy + "_block_dint " + collection_basename + " " + output + ".bin"
+    cmd += " 2> " + output + ".log"
     os.system("cmd")
