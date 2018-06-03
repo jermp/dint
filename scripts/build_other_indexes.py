da16be8e4fdb996bcbce4c2b10391a0289321b26
Giulio Ermanno Pibiri <jeis90@gmail.com>
2018-06-03 12:17:32 +1000

    scripts/build_other_indexes.py

--- scripts/build_other_indexes.py
+++ scripts/build_other_indexes.py
@@ -3,20 +3,11 @@ import sys, os
 collection_basename = sys.argv[1]
 ouput_prefix_name = sys.argv[2]
 
-strategies =
-            [
-            "block_vbyte",
-            "block_varint",
-            "block_optpfor",
-            "block_interpolative",
-            "block_qmx",
-            "block_simple16",
-            "opt"
-            ]
+strategies = ["block_vbyte", "block_varint", "block_optpfor", "block_interpolative", "block_qmx", "block_simple16", "opt"]
 
 for strategy in strategies:
     output = ouput_prefix_name + "." + strategy
     cmd = "./create_freq_index " + strategy + " " + collection_basename
-    cmd += " 2> " + output + ".log"
+    cmd += " 2> ./competitors/" + output + ".log"
     # print cmd
     os.system(cmd)
