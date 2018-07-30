7bbd64faf615fa6c170b7e09890398fb7d464dfd
Giulio Ermanno Pibiri <jeis90@gmail.com>
2018-07-30 10:22:57 +1000

    build_other_indexes.py updated

--- scripts/build_other_indexes.py
+++ scripts/build_other_indexes.py
@@ -3,7 +3,14 @@ import sys, os
 collection_basename = sys.argv[1]
 ouput_prefix_name = sys.argv[2]
 
-strategies = ["block_vbyte", "block_varint", "block_optpfor", "block_interpolative", "block_qmx", "block_simple16", "opt"]
+strategies = ["block_varintgb",
+              "block_varintg8iu",
+              "block_vbyte",
+              "block_qmx",
+              "block_simple16",
+              "block_optpfor",
+              "opt",
+              "block_interpolative"]
 
 for strategy in strategies:
     output = ouput_prefix_name + "." + strategy
